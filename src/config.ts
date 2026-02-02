62844e84528563f35afde9507c67d9170515429e
AnouarSbia <33187719+AnouarSbia@users.noreply.github.com>
2026-02-02 23:21:27 +0100

    A

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'AnouarLab | Consultant SEO';
+export const SITE_DESCRIPTION = 'AnouarLab - Consultant SEO expert aidant les entreprises à améliorer leur visibilité en ligne et leur positionnement sur les moteurs de recherche. Découvrez des stratégies pour une croissance organique et un succès digital.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
