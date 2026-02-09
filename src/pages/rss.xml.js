288345743858f9c34acb17b03db65c13e07f8ab9
AnouarSbia <33187719+AnouarSbia@users.noreply.github.com>
2026-02-10 00:04:08 +0100

    RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,10 @@
 import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const blog = await getCollection("blog");
@@ -13,6 +17,16 @@ export async function GET(context) {
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+      customData: post.data.heroImage
+        ? `<media:content url="${new URL(post.data.heroImage, context.site)}" medium="image" />`
+        : "",
     })),
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/',
+      atom: 'http://www.w3.org/2005/Atom',
+    },
   });
 }
