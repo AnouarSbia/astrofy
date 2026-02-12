1d009e5cf2a272d455410d751cf5d6ff4385aa4b
AnouarSbia <33187719+AnouarSbia@users.noreply.github.com>
2026-02-12 17:00:14 +0100

    FIX Redirection

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anouarlab.fr',
+  trailingSlash: 'always',
   integrations: [mdx(), sitemap(), tailwind()]
 });
